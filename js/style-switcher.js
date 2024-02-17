f00bf7bafdcf9d26411edc61768ddeae67c65eb1
fsRakib <fsrakiba50@gmail.com>
2024-02-17 23:10:51 +0600

    Highlighted  navigation items

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -36,3 +36,24 @@ window.addEventListener("load", () => {
         dayNight.querySelector("i").classList.add("fa-moon");
     }
 })
+/*================================== Extra ==================================*/
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav li a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").slice(1) === current) {
+            link.classList.add("active");
+        }
+    });
+});
